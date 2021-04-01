facb606d48efef403d947a59c7ce2f08736b7a28
vennypwang <vennypwang@tencent.com>
2021-04-01 10:34:28 +0800

    my

--- src/permission.js
+++ src/permission.js
@@ -5,7 +5,7 @@ import getPageTitle from '@/utils/get-page-title'
 
 NProgress.configure({ showSpinner: false }) // NProgress Configuration
 
-router.beforeEach(async (to, from, next) => {
+router.beforeEach(async(to, from, next) => {
   // start progress bar
   NProgress.start()
   // set page title
